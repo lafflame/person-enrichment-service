9e8b4ad3df6c3ce3e77152dac3c33acfe627fb8f
Alexey <cooldock4play@gmail.com>
2025-06-11 14:36:20 +0300

    Добавлена настройка окружения через env файл

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module mod
 
 go 1.24.3
 
-require github.com/lib/pq v1.10.9 // indirect
+require github.com/lib/pq v1.10.9
+
+require github.com/joho/godotenv v1.5.1
--- main.go
+++ main.go
@@ -5,10 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"net/http"
+	"os"
+	"strconv"
 
+	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
 )
+
+var db *sql.DB
 
 type AgifyResponse struct {
 	Count int    `json:"count"`
@@ -31,21 +37,40 @@ type Country struct {
 	Probability float64 `json:"probability"`
 }
 
-const (
-	DBHost     = "localhost"
-	DBPort     = 5432
-	DBUser     = "postgres"
-	DBPassword = ""
-	DBName     = "users"
-)
-
 func main() {
+	// Загружаем переменные окружения из .env файла
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Ошибка загрузки .env файла")
+	}
+
+	// Инициализация БД
+	db, err = initDB()
+	if err != nil {
+		log.Fatalf("Ошибка подключения к БД: %v", err)
+	}
+	defer db.Close()
+
+	// Проверяем соединение
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Не удалось проверить соединение с БД: %v", err)
+	}
+
+	fmt.Println("Успешное подключение к PostgreSQL!")
+
+	getName()
+
 	var name string
 	for {
-		fmt.Println("\nВведите имя:")
+		fmt.Println("\nВведите имя (или 'exit' для выхода):")
 		fmt.Scan(&name)
-		age := agify(name)
 
+		if name == "exit" {
+			break
+		}
+
+		age := agify(name)
 		if age != 0 {
 			gender := genderize(name)
 			nation := nationalize(name)
@@ -54,21 +79,74 @@ func main() {
 	}
 }
 
-func addingDb(name string, age int, gender string, nation string) {
-	connect := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=disable",
-		DBHost, DBPort, DBUser, DBName)
-	db, err := sql.Open("postgres", connect)
+func initDB() (*sql.DB, error) {
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	dbname := os.Getenv("DB_NAME")
+	sslmode := os.Getenv("DB_SSLMODE")
+	password := os.Getenv("DB_PASSWORD")
 
-	if err != nil {
-		panic(err)
+	// Проверяем, что все обязательные переменные установлены
+	if host == "" || port == "" || user == "" || dbname == "" {
+		return nil, fmt.Errorf("не все обязательные переменные окружения установлены")
 	}
+
+	// Преобразуем порт в число
+	p, err := strconv.Atoi(port)
+	if err != nil {
+		return nil, fmt.Errorf("неверный формат порта: %v", err)
+	}
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s dbname=%s sslmode=%s password=%s",
+		host, p, user, dbname, sslmode, password)
+
+	return sql.Open("postgres", connStr)
+}
+
+func addingDb(name string, age int, gender string, nation string) {
+	initDB()
 	defer db.Close()
 
 	add, err := db.Exec("insert into users (name, age, gender, nation) values ($1, $2, $3, $4)", name, age, gender, nation)
 	if err != nil {
-		panic(err)
+		fmt.Println("Не удалось записать данные в таблицу БД, попробуйте заново")
 	}
-	fmt.Println(add.RowsAffected())
+	fmt.Println(add)
+}
+
+// TODO
+func getName() {
+	// Подключение к базе данных
+	initDB()
+	defer db.Close()
+
+	// Получение имени от пользователя
+	var name string
+	fmt.Println("Введите имя для поиска по базе данных:")
+	fmt.Scan(&name)
+
+	// Выполнение запроса и получение одной записи
+	var id int
+	var foundName string
+	var age int
+	var gender string
+
+	err := db.QueryRow("SELECT id, name, age, gender FROM users WHERE name = $1", name).Scan(
+		&id, &foundName, &age, &gender)
+
+	if err != nil {
+		if err == sql.ErrNoRows {
+			fmt.Printf("Пользователь с именем '%s' не найден\n", name)
+		} else {
+			fmt.Println("Не удалось подключиться к БД, попробуйте заново")
+		}
+		return
+	}
+
+	// Вывод найденного пользователя
+	fmt.Printf("Найден пользователь:\nID: %d\nИмя: %s\nВозраст: %d\nПол: %s\n",
+		id, foundName, age, gender)
 }
 
 func agify(name string) int {
